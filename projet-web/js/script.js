1c44f57ac00361f2783b14d47d540b4bd7841f47
Théo Vande-Rosieren <theosieren@gmail.com>
2026-02-20 14:41:45 +0100

    reglage bug

--- projet-web/js/script.js
+++ projet-web/js/script.js
@@ -163,11 +163,6 @@ themeBtn.addEventListener('click', () => {
     let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
     htmlElement.setAttribute('data-theme', newTheme);
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> c86accde16f0a02098f65054e9ab271033825013
     localStorage.setItem('theme', newTheme);
 
     themeBtn.innerText = newTheme === 'dark' ? '☀️' : '🌙';
