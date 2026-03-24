98e5f994b34969ad1d27610f687fb0bb69e0498a
rabah benabdelaziz <predatorsraboul@gmail.com>
2026-03-24 10:18:50 +0100

    dernier commit

--- projet-web/includes/footer.php
+++ projet-web/includes/footer.php
@@ -7,9 +7,9 @@
         </div>
         <div class="footer-column">
             <p><strong>Campus Paris :</strong> 
-            <a href="https://earth.google.com/web/search/30-32+Av.+de+la+R%c3%a9publique/@48.7885011,2.36363684,101.75355606a,0d,60y,7.72464004h,105.63056706t,0r/data=CooBGlwSVgolMHg0N2U2NzNlMjRlODQ2NWIzOjB4NWY0OTQzMDBmOWMyOGI5YhlHYc239GRIQCEXBQtx9ugCQCobMzAtMzIgQXYuIGRlIGxhIFLDqXB1YmxpcXVlGAEgASImCiQJDH103SFvRkARuQBR9WduRkAZXKTz-_rG4b8hts2qYRIP4r9CAggBIhoKFjFfRVZNWllqMF8yVjkxWmVqY3VuSFEQAjoDCgEwQgIIAEoNCP___________wEQAA" target="_blank">30-32 Av. de la République, Villejuif</a></p>
+            <a href="https://earth.google.com/web/search/30-32+Av.+de+la+R%c3%a9publique/@48.7885011,2.36363684,101.75355606a,0d,60y,7.72464004h,105.63056706t,0r/data=CooBGlwSVgolMHg0N2U2NzNlMjRlODQ2NWIzOjB4NWY0OTQzMDBmOWMyOGI5YhlHYc239GRIQCEXBQtx9ugCQCobMzAtMzIgQXYuIGRlIGxhIFLDqXB1YmxpcXVlGAEgASImCiQJDH103SFvRkARuQBR9WduRkAZXKTz-_rG4b8hts2qYRIP4r9CAggBIhoKFjFfRVZNWllqMF8yVjkxWmVqY3VuSFEQAjoDCgEwQgIIAEoNCP___________wEQAA" style="color:white;" target="_blank">30-32 Av. de la République, Villejuif</a></p>
             <p><strong>Campus Bordeaux :</strong> 
-            <a href="https://earth.google.com/web/search/83+Rue+Lucien+Faure,+33000+Bordeaux/@44.86538428,-0.55993967,6.21398208a,0d,88.33793024y,37.08977812h,97.98464222t,0r/data=CpEBGmMSXQokMHhkNTUyODhiNzY4OWEzZGY6MHhjYjEyMmYxMzYwNGI2NDJmGQ34oXfPbkZAIdvzobAw6eG_KiM4MyBSdWUgTHVjaWVuIEZhdXJlLCAzMzAwMCBCb3JkZWF1eBgBIAEiJgokCQilxB9bSkhAEab6wEotSkhAGcLiKi-BbQJAIZRn5O2OYwJAQgIIASIaChZteEdDMUtfQlZWZ09IdGJVSkZEVGZnEAI6AwoBMEICCABKDQj___________8BEAA" target="_blank">83 Rue Lucien Faure, 33000 Bordeaux</a></p>
+            <a href="https://earth.google.com/web/search/83+Rue+Lucien+Faure,+33000+Bordeaux/@44.86538428,-0.55993967,6.21398208a,0d,88.33793024y,37.08977812h,97.98464222t,0r/data=CpEBGmMSXQokMHhkNTUyODhiNzY4OWEzZGY6MHhjYjEyMmYxMzYwNGI2NDJmGQ34oXfPbkZAIdvzobAw6eG_KiM4MyBSdWUgTHVjaWVuIEZhdXJlLCAzMzAwMCBCb3JkZWF1eBgBIAEiJgokCQilxB9bSkhAEab6wEotSkhAGcLiKi-BbQJAIZRn5O2OYwJAQgIIASIaChZteEdDMUtfQlZWZ09IdGJVSkZEVGZnEAI6AwoBMEICCABKDQj___________8BEAA" style="color:white;" target="_blank">83 Rue Lucien Faure, 33000 Bordeaux</a></p>
         </div>
     </div>
     <div class="footer-bottom">
